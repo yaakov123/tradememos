ddbc8033aee69f893cec96361b576d6f160696ab
Yaakov Beckerman <beckermanyaakov@gmail.com>
2023-11-05 10:14:31 +0200

    add primevue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  modules: ["nuxt-primevue", "@nuxtjs/tailwindcss"],
+  css: ["primevue/resources/themes/bootstrap4-light-blue/theme.css"],
+  primevue: {
+    cssLayerOrder: "primevue",
+    usePrimeVue: true,
+  },
+});
